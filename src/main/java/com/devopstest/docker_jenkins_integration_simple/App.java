2c3959f7016051e44aab4dd984204303086b8702
chahid chirchi <chahid23chirchi@gmail.com>
2021-01-04 00:42:10 +0100

    first commit

---
+++ src/main/java/com/devopstest/docker_jenkins_integration_simple/App.java
@@ -0,0 +1,13 @@
+package com.devopstest.docker_jenkins_integration_simple;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
